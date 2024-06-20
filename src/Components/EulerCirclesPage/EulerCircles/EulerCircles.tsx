df6a46cbc67e6b70838564f436ce9665121c791c
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2024-06-21 02:34:50 +0500

    .

--- src/Components/EulerCirclesPage/EulerCircles/EulerCircles.tsx
+++ src/Components/EulerCirclesPage/EulerCircles/EulerCircles.tsx
@@ -8,60 +8,60 @@ interface IEulerCircles {
     theme: string;
 }
 
+function resize(table: TruthTable, theme: string) {
+    const canvas: HTMLCanvasElement = document.getElementById(
+        'canvas'
+    ) as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d')!;
+    let fontSize;
+    function resizeCanvas() {
+        if (window.innerWidth >= 1300) {
+            canvas.width = window.innerWidth * 0.4;
+            canvas.height = window.innerHeight * 0.65;
+            fontSize = 40;
+        } else if (window.innerWidth >= 1000) {
+            canvas.width = window.innerWidth * 0.4;
+            canvas.height = window.innerHeight * 0.65;
+            fontSize = 30;
+        } else if (window.innerWidth >= 700) {
+            canvas.width = window.innerWidth * 0.8;
+            canvas.height = window.innerHeight * 0.65;
+            fontSize = 25;
+        } else if (window.innerWidth >= 400) {
+            canvas.width = window.innerWidth * 0.8;
+            canvas.height = window.outerHeight * 0.5;
+            fontSize = 20;
+        } else {
+            canvas.width = window.innerWidth * 0.82;
+            canvas.height = window.outerHeight * 0.45;
+            fontSize = 15;
+        }
+        draw(
+            ctx,
+            table,
+            canvas.width,
+            canvas.height,
+            theme,
+            3,
+            Math.min(canvas.width, canvas.height) / 4,
+            fontSize
+        );
+    }
+    resizeCanvas();
+}
+
 const EulerCircles = (props: IEulerCircles) => {
     const table = props.table;
 
     useEffect(() => {
         if (table.variables.length !== 0) {
-            resize();
+            resize(table, props.theme);
         }
-    }, [table]);
+    }, [table, props.theme]);
 
     useEffect(() => {
-        resize();
-    }, [props.theme]);
-
-    function resize() {
-        const canvas: HTMLCanvasElement = document.getElementById(
-            'canvas'
-        ) as HTMLCanvasElement;
-        const ctx = canvas.getContext('2d')!;
-        let fontSize;
-        function resizeCanvas() {
-            if (window.innerWidth >= 1300) {
-                canvas.width = window.innerWidth * 0.4;
-                canvas.height = window.innerHeight * 0.65;
-                fontSize = 40;
-            } else if (window.innerWidth >= 1000) {
-                canvas.width = window.innerWidth * 0.4;
-                canvas.height = window.innerHeight * 0.65;
-                fontSize = 30;
-            } else if (window.innerWidth >= 700) {
-                canvas.width = window.innerWidth * 0.8;
-                canvas.height = window.innerHeight * 0.65;
-                fontSize = 25;
-            } else if (window.innerWidth >= 400) {
-                canvas.width = window.innerWidth * 0.8;
-                canvas.height = window.outerHeight * 0.5;
-                fontSize = 20;
-            } else {
-                canvas.width = window.innerWidth * 0.82;
-                canvas.height = window.outerHeight * 0.45;
-                fontSize = 15;
-            }
-            draw(
-                ctx,
-                table,
-                canvas.width,
-                canvas.height,
-                props.theme,
-                3,
-                Math.min(canvas.width, canvas.height) / 4,
-                fontSize
-            );
-        }
-        resizeCanvas();
-    }
+        resize(table, props.theme);
+    }, [table, props.theme]);
 
     return (
         <div className={styles.container} id="capture_circles">
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,24 +1,30 @@
-import s from "./Navbar.module.css";
-import menu_img from "../../static/images/menu.svg";
-import close from "../../static/images/close.svg";
-import rightArrow from "../../static/images/rightArrow.svg";
-import { useCallback, useContext, useState } from "react";
-import { TabContext } from "providers/TabProvider/TabContext";
+import s from './Navbar.module.css';
+import menu_img from '../../static/images/menu.svg';
+import close from '../../static/images/close.svg';
+import rightArrow from '../../static/images/rightArrow.svg';
+import { useCallback, useContext, useState } from 'react';
+import { TabContext } from 'providers/TabProvider/TabContext';
 
 const Navbar = ({ switchTheme, theme }: any) => {
     const [showMobileNav, setShowMobileNav] = useState(false);
     const { setTab } = useContext(TabContext);
 
-    const changeTab = useCallback((tab: number) => {
-        return () => setTab(tab);
-    }, [setTab]);
+    const changeTab = useCallback(
+        (tab: number) => {
+            return () => setTab(tab);
+        },
+        [setTab]
+    );
 
-    const mobileClickHandler = useCallback((tab: number) => {
-        return () => {
-            setTab(tab);
-            setShowMobileNav(false);
-        };
-    },[]);
+    const mobileClickHandler = useCallback(
+        (tab: number) => {
+            return () => {
+                setTab(tab);
+                setShowMobileNav(false);
+            };
+        },
+        [setTab]
+    );
 
     return (
         <>
@@ -30,7 +36,10 @@ const Navbar = ({ switchTheme, theme }: any) => {
                     <div className={s.navLink} onClick={changeTab(1)}>
                         Логический решатель
                     </div>
-                    <div className={s.mobile_menu} onClick={() => setShowMobileNav(true)}>
+                    <div
+                        className={s.mobile_menu}
+                        onClick={() => setShowMobileNav(true)}
+                    >
                         <img src={menu_img} alt="" />
                     </div>
                     <div className={s.dark_mode_container}>
@@ -38,7 +47,7 @@ const Navbar = ({ switchTheme, theme }: any) => {
                         <label className={s.switch}>
                             <input
                                 type="checkbox"
-                                checked={theme === "dark"}
+                                checked={theme === 'dark'}
                                 onChange={() => switchTheme(theme)}
                             />
                             <span className={`${s.slider} ${s.round}`}></span>
@@ -51,8 +60,15 @@ const Navbar = ({ switchTheme, theme }: any) => {
                     <div className={s.mobile_nav}>
                         <div className={s.left_side_mobile}></div>
                         <div className={s.right_side}>
-                            <div className={s.close} onClick={() => setShowMobileNav(false)}>
-                                <img src={close} alt="" className={s.right_arrow_svg} />
+                            <div
+                                className={s.close}
+                                onClick={() => setShowMobileNav(false)}
+                            >
+                                <img
+                                    src={close}
+                                    alt=""
+                                    className={s.right_arrow_svg}
+                                />
                             </div>
                             <ul className={s.mobile_links}>
                                 <div
